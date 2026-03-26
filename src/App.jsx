aa331d09af4241c7df228557b73589a9353baea8
BhavyaLuhana <shayamsundarluhana609@gmail.com>
2026-03-27 00:53:49 +0530

    adding base in vite.config.js and adding projects in projects sectin

--- src/App.jsx
+++ src/App.jsx
@@ -45,9 +45,14 @@ const App = () => {
 
   const projects = [
     {
-      title: "Project Management System",
-      description: "A full-stack MERN-based system that allows users to create, update, and manage projects efficiently. It includes a responsive UI built with Chakra UI and supports CRUD operations..",
-      tech: "MERN, Chakra UI"
+      title: "Sign Language Learning App (SIH Finalist Project)",
+      description: "An AI-powered full-stack application designed to help users learn Indian Sign Language using an interactive library, quizzes, and search functionality. The system uses MERN architecture with a FastAPI-based ML microservice and Cloudinary for media handling, enabling scalable learning and future sign recognition features.",
+      tech: "MERN, FastAPI, AI/ML, Cloudinary"
+    },
+    {
+      title: "Research Project – Evolution of Online Communities using Graph Neural Networks",
+      description: "A research-driven project focused on detecting and predicting the evolution of online communities in dynamic social networks. It leverages Graph Neural Networks (GCN), clustering techniques, and temporal modeling to analyze community behavior over time.",
+      tech: "Python, GNN (GCN), GRU, Network Analysis, Machine Learning"
     },
     {
       title: "Log File Explainer Tool",
@@ -55,9 +60,19 @@ const App = () => {
       tech: "React, OpenAI API, Express"
     },
     {
+      title: "ChatSecure – A Privacy-Focused Encrypted Messaging App",
+      description: "ChatSecure is a real-time messaging application built with a strong focus on end-to-end encryption, privacy, and security. Designed to offer seamless and secure communication, it ensures that all messages are encrypted before being sent and decrypted only by the recipient.",
+      tech: "MERN (MongoDB, Express.js, React, Node.js), WebSockets, CryptoJS, Tailwind CSS"
+    },
+    {
       title: "Regex-based Web Scraper",
       description: "A lightweight web scraper built using Java and Regular Expressions (Regex) to extract relevant data from web pages. Instead of relying on full-fledged libraries like JSoup, this scraper processes raw HTML using regex patterns to extract links, headings, and text content.",
       tech: "Java, Regex"
+    },
+    {
+      title: "Project Management System",
+      description: "A full-stack MERN-based system that allows users to create, update, and manage projects efficiently. It includes a responsive UI built with Chakra UI and supports CRUD operations..",
+      tech: "MERN, Chakra UI"
     }
   ];
 
